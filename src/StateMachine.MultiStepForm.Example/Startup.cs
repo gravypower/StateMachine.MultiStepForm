7b1c0292f4dd4c5ea0ff4ae50ae3c6af589349ea
Gravypower <aaron@gravypower.net>
2020-07-03 12:15:27 +1000

    Updated references and framework

--- src/StateMachine.MultiStepForm.Example/Startup.cs
+++ src/StateMachine.MultiStepForm.Example/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using SimpleInjector;
 using SimpleInjector.Integration.AspNetCore.Mvc;
 using SimpleInjector.Lifestyles;
@@ -25,32 +26,57 @@ namespace StateMachine.MultiStepForm.Example
     public class Startup
     {
         public static readonly Container Container = new Container();
+        public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddSessionStateTempDataProvider();
+            services.AddControllersWithViews();
             services.AddSession();
+            
+            services.AddLogging();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
+            // Sets up the basic configuration that for integrating Simple Injector with
+            // ASP.NET Core by setting the DefaultScopedLifestyle, and setting up auto
+            // cross wiring.
+            services.AddSimpleInjector(Container, options =>
+            {
+                // AddAspNetCore() wraps web requests in a Simple Injector scope and
+                // allows request-scoped framework services to be resolved.
+                options.AddAspNetCore()
 
-            IntegrateSimpleInjector(services);
+                    // Ensure activation of a specific framework type to be created by
+                    // Simple Injector instead of the built-in configuration system.
+                    // All calls are optional. You can enable what you need. For instance,
+                    // ViewComponents, PageModels, and TagHelpers are not needed when you
+                    // build a Web API.
+                    .AddControllerActivation()
+                    .AddViewComponentActivation()
+                    .AddPageModelActivation()
+                    .AddTagHelperActivation();
 
+                // Optionally, allow application components to depend on the non-generic
+                // ILogger (Microsoft.Extensions.Logging) or IStringLocalizer
+                // (Microsoft.Extensions.Localization) abstractions.
+                options.AddLogging();
+                options.AddLocalization();
+            });
+            
+            InitializeContainer();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            InitialiseContainer(app);
-
-            Container.Verify(VerificationOption.VerifyAndDiagnose);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -58,63 +84,47 @@ namespace StateMachine.MultiStepForm.Example
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
+            
+            app.UseSimpleInjector(Container);
 
+            // Default ASP.NET middleware
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseRouting();
+            
             app.UseSession();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    "Default", // Route name 
-                    "{controller}/{action}/{id}", // URL with parameters 
-                    new {controller = "Home", action = "Index", id = ""} // Parameter defaults
-                );
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            
+            Container.Verify();
         }
-
-        private void IntegrateSimpleInjector(IServiceCollection services)
-        {
-            Container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-            Container.Options.DefaultLifestyle = Lifestyle.Scoped;
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            services.AddSingleton<IControllerActivator>(
-                new SimpleInjectorControllerActivator(Container));
-            services.AddSingleton<IViewComponentActivator>(
-                new SimpleInjectorViewComponentActivator(Container));
-
-            services.EnableSimpleInjectorCrossWiring(Container);
-            services.UseSimpleInjectorAspNetRequestScoping(Container);
-        }
-
-        private void InitialiseContainer(IApplicationBuilder app)
+        private void InitializeContainer()
         {
             // Add application presentation components:
-            Container.RegisterMvcControllers(app);
-            Container.RegisterMvcViewComponents(app);
-
             Container.Register<ICommandHandler<SubmitYourQuestion>, SubmitYourQuestionCommandHandler>();
             Container.Register<IQueryHandler<GetYourQuestion, string>, GetYourQuestionQueryHandler>();
             
             Container.Register<AbstractStateMachine<DeepThoughtStates.State, DeepThoughtTriggers.Trigger>, DeepThoughtStateMachine>();
-
+            
             Container.RegisterDecorator(typeof(ICommandHandler<>), typeof(VerboseLoggingCommandHandlerDecorator<>));
-
+            
             var assembly = GetType().Assembly;
             RegisterScopedCollection<State>(assembly);
             RegisterScopedCollection<Trigger>(assembly);
-
+            
             Container.Register<StateContext>();
             Container.Register<TriggerContext>();
             Container.Register<Specification<AnswerViewModel>, MeaningOfLifeSpecification>();
-
-            // Allow Simple Injector to resolve services from ASP.NET Core.
-            Container.AutoCrossWireAspNetComponents(app);
         }
-
+        
         private void RegisterScopedCollection<TType>(Assembly assembly)
         {
             var registrationTypes = Container.GetTypesToRegister(typeof(TType), assembly);
@@ -122,6 +132,7 @@ namespace StateMachine.MultiStepForm.Example
             {
                 var registration = Lifestyle.Scoped.CreateRegistration(registrationType, Container);
                 Container.Collection.Append(typeof(TType), registration);
+                Container.AddRegistration(registrationType, registration);
             }   
         }
     }
